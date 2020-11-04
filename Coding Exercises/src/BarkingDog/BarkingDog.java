9d2467b84534063b1f27a85df4fc9d27417c5611
Anestis Giannakosian <anestidus@hotmail.com>
2020-11-04 15:37:31 +0200

    barking Dog finished

--- Coding Exercises/src/BarkingDog/BarkingDog.java
+++ Coding Exercises/src/BarkingDog/BarkingDog.java
@@ -1,7 +1,7 @@
 package BarkingDog;
 
 public class BarkingDog {
-
+    /** if on line 8 can be simplified --> return barking && hourOfDay < 8 || hourOfDay > 22;*/
     public static boolean shouldWakeUp(boolean barking , int hourOfDay) {
         if (hourOfDay < 0 || hourOfDay > 23) {
             return false;
