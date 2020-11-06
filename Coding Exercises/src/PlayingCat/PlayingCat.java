64be766e7ece8042b730ff39e4fa2f8fcfafd1d7
Anestis Giannakosian <anestidus@hotmail.com>
2020-11-06 17:47:27 +0200

    finished section 4,going to section 5 flow control

--- Coding Exercises/src/PlayingCat/PlayingCat.java
+++ Coding Exercises/src/PlayingCat/PlayingCat.java
@@ -4,9 +4,6 @@ public class PlayingCat {
 
     public static boolean isCatPlaying(boolean summer, int temperature){
 
-        if ( (!summer && temperature>=25 && temperature<=35) || (summer && temperature>=25 && temperature<=45) ) {
-            return true;
-        }
-            return false;
-        }
+        return (!summer && temperature >= 25 && temperature <= 35) || (summer && temperature >= 25 && temperature <= 45);
+    }
 }
